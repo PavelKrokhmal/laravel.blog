4c21cdbcb650709677e5e18b5521f7ccf73f2ec1
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-07-25 11:35:22 +0300

    admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix'=>'admin', 'namespace'=>'App\Http\Controllers\Admin'], function () {
+    Route::get('/', 'MainController@index')->name('admin.index');
+});
+
