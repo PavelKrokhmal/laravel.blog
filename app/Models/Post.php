e186ce51cda67c06bfc29a702f33e8f147804230
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-07-25 12:28:53 +0300

    migrations & entities relationship

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use Sluggable;
+
+    public function tags()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
